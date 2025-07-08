a609264f9ab98c7ad562313bf0f53ce0cde6b079
Ryan <kinisonryan@outlook.com>
2025-07-08 16:16:58 -0600

    A

--- Program.cs
+++ Program.cs
@@ -6,52 +6,65 @@ namespace Assignment1._2._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter your first number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            bool again = true;
+            do
+            { 
+                Console.WriteLine("Please Enter your first number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please Enter symbol of desired operation");
-            string operation = Console.ReadLine();
+                Console.WriteLine("Please Enter symbol of desired operation");
+                string operation = Console.ReadLine();
 
-            Console.WriteLine("Please enter your second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter your second number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
-            int solution = 0;
-            bool complete = false;
-            while (!complete)
-            {
-                switch (operation)
+                int solution = 0;
+                bool complete = false;
+                while (!complete)
                 {
-                    case "+":
-                        solution = num1 + num2;
-                        complete = true;
-                        Console.WriteLine(num1 + "+" + num2 + "=" + solution);
-                        break;
+                    switch (operation)
+                    {
+                        case "+":
+                            solution = num1 + num2;
+                            complete = true;
+                            Console.WriteLine(num1 + "+" + num2 + "=" + solution);
+                            break;
 
-                    case "-":
-                        solution = num1 - num2;
-                        complete = true;
-                        Console.WriteLine(num1 + "-" + num2 + "=" + solution);
-                        break;
+                        case "-":
+                            solution = num1 - num2;
+                            complete = true;
+                            Console.WriteLine(num1 + "-" + num2 + "=" + solution);
+                            break;
 
-                    case "*":
-                        solution = num1 * num2;
-                        complete = true;
-                        Console.WriteLine(num1 + "*" + num2 + "=" + solution);
-                        break;
+                        case "*":
+                            solution = num1 * num2;
+                            complete = true;
+                            Console.WriteLine(num1 + "*" + num2 + "=" + solution);
+                            break;
 
-                    case "/":
-                        solution = num1 / num2;
-                        complete = true;
-                        Console.WriteLine(num1 + "/" + num2 + "=" + solution);
-                        break;
-                    default:
-                        Console.WriteLine("Please enter a valid math operator such as (+, -, /, *)");
-                        operation = Console.ReadLine();
+                        case "/":
+                            solution = num1 / num2;
+                            complete = true;
+                            Console.WriteLine(num1 + "/" + num2 + "=" + solution);
+                            break;
+                        default:
+                            Console.WriteLine("Please enter a valid math operator such as (+, -, /, *)");
+                            operation = Console.ReadLine();
 
-                        break;
+                            break;
+                    }
                 }
-            }
+                Console.WriteLine("Would you like to perform another operation? (Y/N");
+                if (Console.ReadLine() == "y" || Console.ReadLine() == "Y")
+                {
+                    again = true;
+                }
+                else
+                {
+                    again = false;
+                }
 
+            } while (again);
         }
     }
 }
