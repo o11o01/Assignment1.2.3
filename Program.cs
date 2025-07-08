9d2231749336b51b3b9e234a93a627764d1c3ef6
Ryan <kinisonryan@outlook.com>
2025-07-08 15:20:36 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.Design;
+
+namespace Assignment1._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter symbol of desired operation");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Please enter your second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int solution = 0;
+            bool complete = false;
+            while (!complete)
+            {
+                switch (operation)
+                {
+                    case "+":
+                        solution = num1 + num2;
+                        complete = true;
+                        Console.WriteLine(num1 + "+" + num2 + "=" + solution);
+                        break;
+
+                    case "-":
+                        solution = num1 - num2;
+                        complete = true;
+                        Console.WriteLine(num1 + "-" + num2 + "=" + solution);
+                        break;
+
+                    case "*":
+                        solution = num1 * num2;
+                        complete = true;
+                        Console.WriteLine(num1 + "*" + num2 + "=" + solution);
+                        break;
+
+                    case "/":
+                        solution = num1 / num2;
+                        complete = true;
+                        Console.WriteLine(num1 + "/" + num2 + "=" + solution);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid math operator such as (+, -, /, *)");
+                        operation = Console.ReadLine();
+
+                        break;
+                }
+            }
+
+        }
+    }
+}
